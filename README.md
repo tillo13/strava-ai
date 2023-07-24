b933338e801e72eeb611fee76a761c4e09a07735
Andy Tillo <andy.tillo@gmail.com>
2023-07-24 15:08:02 -0700

    add blog post, update readme

--- README.md
+++ README.md
@@ -1,46 +1,45 @@
-# Strava Workout Analyzer
+# Strava Workout Analyzer + Machine Learning Exploration
 
-This is a suite of Python scripts for the Strava fitness application. The aim is to download, cleanse and analyze user activities in different ways. There are scripts to access the Strava API, fetch and refresh access tokens, download activities, validate JSON data and classify, score and provide summary statistics about activities.
+This digital suite, crafted from a collection of Python scripts, is a specialized toolkit for the Strava fitness application. Its primary purpose is to download, cleanse, and perform a meticulous analysis of a user's ‘Strava activities’. It is enriched with scripts that serve a variety of utilities - connecting with the Strava API, fetching and refreshing access tokens, downloading activities, validating the reliability of JSON data, and eventually providing classified, scored, and wholesome summaries of the fitness activities.
 
-### List and value of some scripts:
-1. **validate_json.py**: 
-This script takes JSON files containing cycling activities and validates those files. It counts the occurrences of each field, checks for missing fields and prints a summary report.
-   
-2. **initialize.py**: 
-This script initializes the process of fetching a new token from the Strava API, checking for existing tokens and validating them. It also logs the token checking process for future reference.
-   
-3. **last_n_activities.py**: 
-It downloads the last few activities from Strava, and stores each activity as a separate JSON file. The script checks for duplicates and only saves new activities to the directory.
-   
-4. **monthly_totals_over_time.py**: 
-It scans the downloaded activities, categorizes them by the activity type and provides a summary count of them in the corresponding folder.
-   
+Notably equipped for Strava data handling and analysis, the repository carries the following scripts:
+
+1. **initialize.py**: 
+Here's where the journey begins. By sending a GET request to the Strava API, this script fetches a new access token, performs checks for existing ones, validates them, and, if necessary, refreshes them. For audit and debugging purposes, all token-checking activities are duly logged.
+
+2. **last_n_activities.py**: 
+This utility script reaches out to the Strava API to retrieve the most recent activities. Each activity is downloaded, earmarked with its respective activity_id, and saved as an individual JSON file. Additionally, it ensures no activity is copied twice, preventing duplications, and storing only new additions.
+
+3. **validate_json.py**: 
+This script is tasked with validating the downloaded JSON files bearing cycling activities. It scans through each field in every file, counts their occurrences, verifies the presence of all necessary fields, and ultimately, provides a detailed transparency report.
+
+4. **strava_activity_classification.py**: 
+Resulting from this script is the systematic classification of Peloton classes. Equipped with tailored algorithms to track sub-types of generic classifications such as "Workout," it is designed to meticulously separate other platform activities, distinguishing between those logged by ELEMNT BOLT, Zwift, Garmin Ping, Strava Direct, amongst others.
+
 5. **top_peloton_rides.py**: 
-This script finds the top 5 Peloton rides in your Strava history based on a scoring system that measures effort exerted during the ride.
-   
-6. **strava_activity_classification.py**: 
-This script classifies Peloton activities and contains logic to track sub-types of more generic categories, such as "Workout". It also classifies other platforms like ELEMNT BOLT, Zwift, Garmin Ping, Strava Direct, and others.
+This script is your personal leaderboard maker. It navigates through your history of Peloton rides on Strava to rank your top 5 rides. The scoreboard is curated based on a scoring system that calculates the effort exerted during each ride.
 
-7. **briskr_score.py**: 
-This script is used to rank and score rides based on the amount of effort spent using Bris.kr score method. It provides a summary of the top 5 rides for each length of ride.
+6. **briskr_score.py**: 
+Borrowing from the popular Bris.kr score method, this script evaluates the ride intensity, honoring the highest scores to the most intense rides. It nets out with a compiled summary of your top 5 rides for each ride length category, celebrating the sweat and gears of each endeavor.
 
-8. **monthly_totals_over_time.py**: 
-This script scans the downloaded activities, categorizes them by the activity type and month, and provides a summary count of activities for each month. 
+7. **monthly_totals_over_time.py**: 
+This script is your timeline navigator. It rummages through the downloaded activities, assorting them by activity type and month before outputting a wholesome summary. For each month, it tallies up the total number of unique activities.
 
-## Setup
-1. Clone this repository to your local system.
-2. Create a `.env` file in the same directory.
-3. Inside `.env`, add your Strava Client ID and Client Secret as follows:
+## Preparation and Setup
+Please follow the steps below to prepare your environment for the Strava Workout Analyzer:
+
+1. Start by cloning this repository to your local system.
+2. You'll need to create a `.env` file in the root directory.
+3. Inside `.env`, include your Strava Client ID and Client Secret:
 
 ```sh
 STRAVA_CLIENT_ID=your_client_id
 STRAVA_CLIENT_SECRET=your_client_secret
 ```
+Substitute `your_client_id` and `your_client_secret` with your actual credential details.
 
-Replace `your_client_id` and `your_client_secret` with your actual Client ID and Client Secret.
-
-### Python Libraries Used:
-This project makes use of following Python libraries:
+### Internal Workings - Python Libraries Used:
+Running behind the scenes of this project are the under-mentioned Python libraries:
 - json
 - os
 - glob
@@ -55,23 +54,25 @@ This project makes use of following Python libraries:
 - tabulate
 - re
 
-### Instructions to Run most of it:
-1. Make sure that you have Python 3 installed on your machine.
-2. Install the necessary Python dependencies as mentioned in the "Python Libraries Used" section above with pip using command `pip install <library_name>`.
+### How to Run these Scripts:
+1. Please ensure that Python 3 is installed on your system.
+2. Use pip to install the necessary Python dependencies, mentioned above, with the command `pip install <library_name>`.
 3. Add your project directory to your PYTHONPATH environment variable.
 4. Run the required script with the command `python <filename.py>`.
 
-### Future Scope:
-Hit me up with ideas, but a few things: 
-- Add a graphical interface for better user interaction.
-- More refined activity categorization based on user input.
-- Addition of a weather interaction module to adjust the score based on weather conditions.
-- Introduce more metrics to evaluate the performance of users.
+### What the Future Holds:
+Hit me up with your valuable ideas for refining this project. Tentative directions where the project can expand include:
+- Incorporation of a graphical interface to enhance user interaction.
+- Improved activity categorization based on user input.
+- Introduction of a weather interaction module, enabling score adjustments based on prevailing weather conditions.
+- Enumeration of more performance evaluation metrics for further user insights.
 
-## Limit Rate
-Strava API has a limit of 100 requests per 15 minutes and 1000 requests per day. If the limit is exceeded, be friendly to their API!
+## API Rate Limiting
+Please be mindful of the restrictions imposed by Strava API. It has a limit of 100 requests per 15 minutes and 1000 requests in 24 hours. Exceeding the limit is considered unfriendly behavior towards their API!
 
-# Screenshots
+# An Illustrated Summary through Screenshots
+
+Take a tour of these screenshots to understand the visual outputs of the scripts.
 
 ![Top Rides](./images/top_rides.png)
 ![Average KJ](./images/avg_kj.png)
--- data_cleansing/strava_ml.py
+++ data_cleansing/strava_ml.py
@@ -1,131 +1,70 @@
+import os
 import json
 import pandas as pd
-import os
-import glob
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.cluster import KMeans
-from scipy import stats
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+from termcolor import colored
+import numpy as np
 
-# This removes any anomalies from the top and bottom of the data that may skew averages.  0.05 will trim the top and bottom 5% of data for example.
-TRIM_VALUE = 0.002
 
-# These are the fields we think are important in a rider's effort
-numeric_fields = {
-    "distance": None,
-    "moving_time": None,
-    "average_speed": None,
-    "average_watts": None,
-    "max_watts": None,
-    "suffer_score": None
-}
-
-categorical_fields = {
-    "sport_type": None
-}
-
-fields = {**numeric_fields, **categorical_fields}
-
-folder = '/data_cleansing/activities'  
-files = glob.glob(os.path.join(folder, '*.json'))
-
-def trimmed_info(data, proportion_trimmed):
-    sorted_data = np.sort(data)
-    lower_trim_index = int(np.floor(proportion_trimmed * len(sorted_data)))
-    upper_trim_index = int(np.ceil((1 - proportion_trimmed) * len(sorted_data)))
-    trimmed_data = sorted_data[lower_trim_index:upper_trim_index]
-    lower_trimmed_data = sorted_data[:lower_trim_index]
-    upper_trimmed_data = sorted_data[upper_trim_index:]
-    return trimmed_data, lower_trimmed_data, upper_trimmed_data
+folder = './data_cleansing/activities'
+files = os.listdir(folder)
 
 data_list = []
-missing_files = 0
-for file in files:
-    with open(file, 'r') as f:
-        json_data = json.load(f)
-        if any(field not in json_data for field in fields.keys()):
-            missing_files += 1  
-            continue  
-        single_row = {}
-        for field_name, missing_value in fields.items():
-            single_row[field_name] = json_data.get(field_name, missing_value)
-        
-        data_list.append(single_row)
+for filename in files:
+    if filename.endswith('.json'):
+        with open(os.path.join(folder, filename), 'r') as file:
+            try:
+                data = json.load(file)
+                data_dict = {
+                    'average_speed': data.get('average_speed'),
+                    'max_speed': data.get('max_speed'),
+                    'average_cadence': data.get('average_cadence'),
+                    'average_watts': data.get('average_watts'),
+                    'kilojoules': data.get('kilojoules'),
+                    'moving_time': data.get('moving_time'),
+                    'total_elevation_gain': data.get('total_elevation_gain'),
+                    'kudos_count': data.get('kudos_count'),
+                    'max_watts': data.get('max_watts'),
+                    'weighted_average_watts': data.get('weighted_average_watts'),
+                    'suffer_score': data.get('suffer_score'),
+                    'average_heartrate': data.get('average_heartrate')
+                }
+                
+                if not any(v is None for v in data_dict.values()):
+                    data_list.append(data_dict)
+                    
+            except (KeyError, ValueError, UnicodeDecodeError) as e:
+                continue
 
-if missing_files > 0:
-    print(f"{missing_files} files were skipped due to incomplete data.")
-else:
-    print("No files were skipped as all had complete data.")
+if not data_list:
+    raise ValueError("No data found in the provided files.")
 
-df = pd.DataFrame(data_list)
-df["moving_time"] = df["moving_time"]/60
+dataset = pd.DataFrame(data_list)
 
-for field_name in numeric_fields:
-    df[field_name].fillna(df[field_name].mean(), inplace=True)
+# Dropping any rows with missing values
+dataset = dataset.dropna()
 
-required_columns = ['distance', 'average_speed', 'moving_time', 'average_watts']
-if any(column not in df for column in required_columns):
-    print("Could not find all necessary columns in data.")
-    exit()
+features = dataset[['average_speed', 'max_speed', 'average_cadence', 'average_watts', 'kilojoules', 'moving_time', 'total_elevation_gain', 'kudos_count', 'max_watts', 'weighted_average_watts', 'suffer_score']]
+labels = dataset['average_heartrate']
 
-def predict_watts(df, num_clusters=3):
-    model = None
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.2)
 
-    # Cluster the moving_time data
-    kmeans = KMeans(n_clusters=num_clusters, n_init=10)
+print('Linear Regression Results:')
+model = LinearRegression()
+model.fit(features_train, labels_train)
 
-    df['moving_time_cluster'] = kmeans.fit_predict(df['moving_time'].values.reshape(-1, 1))
+labels_pred = model.predict(features_test)
 
-    predictions = {}
-    for cluster in range(num_clusters):
-        # Select instances that belong to the current cluster
-        temp_df = df[df['moving_time_cluster'] == cluster]
-      
-        # Train linear regression 
-        X = temp_df['moving_time'].values.reshape(-1,1)
-        y = temp_df['average_watts']
-        model = LinearRegression().fit(X, y)
+print(colored(f'R-squared: {r2_score(labels_test, labels_pred)}', 'green'))
+print(colored(f'MSE: {mean_squared_error(labels_test, labels_pred)}', 'blue'))
 
-        # Predict for each 10 minute interval in the cluster's ride length range
-        min_ride_length = temp_df['moving_time'].min()
-        max_ride_length = temp_df['moving_time'].max()
+print('\nRandom Forest Regressor Results:')
+model = RandomForestRegressor(n_estimators=100)
+model.fit(features_train, labels_train)
+labels_pred = model.predict(features_test)
 
-        for prediction_minute in np.arange(10, max_ride_length, 10):
-            predictions[(cluster, prediction_minute)] = model.predict(np.array([prediction_minute]).reshape(-1,1))[0]
-
-    return predictions, kmeans.cluster_centers_
-
-predictions, cluster_centers = predict_watts(df)
-
-activity_counts = df['sport_type'].value_counts()
-print("Activity Type Counts:")
-print(activity_counts)
-
-print(f"We analysed {len(df)} of your last rides.")
-print(f"From that {len(df)} rides here is some data:")
-print(f"You average {df['distance'].mean() * 0.000621371:.2f} miles per ride.")
-
-# Calculate 10% trimmed mean of speeds
-average_speed, lower_trimmed_speeds, upper_trimmed_speeds = trimmed_info(df['average_speed'], TRIM_VALUE)
-print(f"You average speed is {average_speed.mean() * 2.237:.2f} mph.")
-print(f"This leaves out the fastest {len(upper_trimmed_speeds)} and slowest {len(lower_trimmed_speeds)} rides.")
-print(f"The fastest speeds trimmed are: {upper_trimmed_speeds * 2.237}")
-print(f"The slowest speeds trimmed are: {lower_trimmed_speeds * 2.237}")
-
-# Calculate TRIM_VALUE% trimmed mean of ride times
-average_time, lower_trimmed_times, upper_trimmed_times = trimmed_info(df['moving_time'], TRIM_VALUE)
-print(f"Your average ride time (trimmed {TRIM_VALUE*100}%) is {average_time.mean():.2f} minutes.")
-print(f"This leaves out the longest {len(upper_trimmed_times)} and shortest {len(lower_trimmed_times)} rides.")
-print(f"The longest times trimmed are: {upper_trimmed_times}")
-print(f"The shortest times trimmed are: {lower_trimmed_times}")
-
-print(f"Your average watts is {df['average_watts'].mean():.2f}W (this is what we'll gauge as an average ride).")
-print(f"Therefore, in the future to get to your {df['average_watts'].mean():.2f}W average ride, based on your past {len(df)} times:")
-
-for cluster, cluster_center in enumerate(cluster_centers):
-    print(f"For a ride length around {cluster_center[0]:.2f} minutes:")
-    min_ride_length = 10
-    max_ride_length = min(df[df['moving_time_cluster'] == cluster]['moving_time'].max(), cluster_center[0] + 10)
-    for prediction_minute in np.arange(min_ride_length, max_ride_length, 10):
-        print(f"You'd need to have your average_watts be {predictions[(cluster, prediction_minute)]:.2f} by minute {prediction_minute}, and you should try to hit those marks.")
+print(colored(f'R-squared: {r2_score(labels_test, labels_pred)}', 'green'))
+print(colored(f'MSE: {mean_squared_error(labels_test, labels_pred)}', 'blue'))
